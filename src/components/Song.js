1e1460d004ca342e22ab108320c455703e229f3b
JParker3110 <156866178+JParker3110@users.noreply.github.com>
2024-10-18 22:32:12 -0400

    update song

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Spotify } from 'react-spotify-embed';
 import StarRating from '../components/StarRating';
+import Link from 'next/link';
 import '/styles/app.css'; 
 
 const Song = ({ link, title }) => {
   const router = useRouter();
   const [rating, setRating] = useState(0);
 
-  const handleClick = () => {
-    router.push({
-      pathname: '/add-review',
-      query: { songLink: link, songTitle: title, songRating: rating },
-    });
-  };
-
   return (
-    <div onClick={handleClick} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', margin: '10px 0' }}>
+    <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0' }}>
       <Spotify link={link} />
       <div className="music-info" style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
         <p className="song-title" style={{ marginRight: '10px', fontSize: '1.5em' }}>{title}</p>
-        <div className="star-rating">
+        <div className="star-rating" style={{ marginRight: '10px' }}>
           <StarRating rating={rating} onRatingChange={setRating} />
         </div>
+        <Link
+          href={{
+            pathname: '/leave-review', 
+            query: { songLink: link, songTitle: title, songRating: rating },
+          }}
+          style={{ textDecoration: 'underline', color: 'blue' }} 
+        >
+          Leave a Review
+        </Link>
       </div>
     </div>
   );
