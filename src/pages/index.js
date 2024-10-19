c6b9337e5a94858fc5219e921fba207b3a5bd503
JParker3110 <156866178+JParker3110@users.noreply.github.com>
2024-10-18 20:52:35 -0400

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,28 @@
-import { useEffect, useState } from 'react';
-import Song from '../components/Song';
+// import { useEffect, useState } from 'react';
+// import Song from '../components/Song';
 
-const Home = ({ backendUrl }) => {
-  const [music, setMusic] = useState([]);
+// const Home = ({ backendUrl }) => {
+//   const [music, setMusic] = useState([]);
 
-  useEffect(() => {
-    const fetchMusic = async () => {
-      const response = await fetch(`${backendUrl}/api/music`);
-      const data = await response.json();
-      setMusic(data);
-    };
+//   useEffect(() => {
+//     const fetchMusic = async () => {
+//       const response = await fetch(`${backendUrl}/api/music`);
+//       const data = await response.json();
+//       setMusic(data);
+//     };
 
-    fetchMusic();
-  }, [backendUrl]);
+//     fetchMusic();
+//   }, [backendUrl]);
 
-  return (
-    <div>
-      {/* <h1>Welcome to the Music Review App!</h1>
-      <h2>My Favorite Tracks</h2> */}
-      {music.map((track) => (
-        <Song key={track.id} link={track.link} title={track.title} />
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Welcome to the Music Review App!</h1>
+//       <h2>My Favorite Tracks</h2>
+//       {music.map((track) => (
+//         <Song key={track.id} link={track.link} title={track.title} />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
